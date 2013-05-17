41af990962fa220028f5b49414280c05cb5bcb6c
good92 <Basile.CHANDESRIS@ext.ec.europa.eu>
2013-05-17 11:58:32 +0200

    v3 - 21 success /37 failed   - ORACLE11G

--- src/main/scala/scala/slick/driver/OracleDriver.scala
+++ src/main/scala/scala/slick/driver/OracleDriver.scala
@@ -3,13 +3,24 @@ package scala.slick.driver
 import java.util.UUID
 import scala.slick.lifted._
 import scala.slick.jdbc.{PositionedParameters, PositionedResult, JdbcType}
-import scala.slick.ast.{SequenceNode, Library, FieldSymbol, Node}
+import slick.ast._
 import scala.slick.util.MacroSupport.macroSupportInterpolation
 import scala.slick.compiler.CompilerState
 
-import java.sql.{Types => SQLTypes}
+import java.sql.{Types => SQLTypes, DatabaseMetaData}
+import scala._
+import collection.mutable.ListBuffer
+import scala.Some
+import slick.ast._
+import slick.ast.FieldSymbol
+import slick.ast.SequenceNode
+import slick.jdbc.meta.MForeignKey
+import slick.profile.{SqlProfile, Capability}
+import scala.Some
+import scala.Some
+
 /**
- * Slick driver for OracleSQL.
+ * Slick driver for OracleSQL - tests on Oracle11G.
  *
  * This driver implements all capabilities of the
  * [[scala.slick.driver.JdbcProfile]].
@@ -25,11 +36,19 @@ import java.sql.{Types => SQLTypes}
  * </ul>
  * http://db.apache.org/ddlutils/databases/oracle.html
  * https://github.com/hibernate/hibernate-orm/tree/master/hibernate-core/src/main/java/org/hibernate/dialect
+ * http://wiki.postgresql.org/wiki/Oracle_to_Postgres_Conversion
  *
- * Status: unstable: 20 success /38 failed
+ * http://jen.fluxcapacitor.net/geek/autoincr.html
+ *
+ * Status: unstable: 21 success /37 failed   - ORACLE11G
  *
  */
 trait OracleDriver extends JdbcDriver { driver =>
+  /* override protected def computeCapabilities: Set[Capability] = (super.computeCapabilities
+    - JdbcProfile.capabilities.returnInsertOther
+    - SqlProfile.capabilities.sequenceLimited
+    ) */
+
 
   override val columnTypes = new JdbcTypes
   override def createQueryBuilder(n: Node, state: CompilerState): QueryBuilder = new QueryBuilder(n, state)
@@ -37,62 +56,66 @@ trait OracleDriver extends JdbcDriver { driver =>
   override def createColumnDDLBuilder(column: FieldSymbol, table: Table[_]): ColumnDDLBuilder = new ColumnDDLBuilder(column)
 
   override def defaultSqlTypeName(tmd: JdbcType[_]): String = tmd.sqlType match {
-    case SQLTypes.ARRAY => "lo" // todo why not "blob"?
-    case SQLTypes.BIGINT => "NUMBER(38)"
-    case SQLTypes.BINARY => "lo" // todo RAW
-    case SQLTypes.BIT => "NUMBER(1)"
-    case SQLTypes.BLOB => "lo"
+    case SQLTypes.ARRAY => "BLOB"
+    case SQLTypes.BIGINT => "NUMBER(19,0)"
+    case SQLTypes.BINARY => "BLOB" // todo RAW
+    case SQLTypes.BIT => "NUMBER(1,0)"
+    case SQLTypes.BLOB => "BLOB"
     case SQLTypes.BOOLEAN  => "NUMBER(1)"
-    case SQLTypes.DATALINK  => "lo"
-    case SQLTypes.DECIMAL  => "NUMBER"
+    case SQLTypes.DATALINK  => "BLOB"
+    case SQLTypes.DECIMAL  => "NUMBER(10,0)"
     case SQLTypes.DOUBLE => "DOUBLE PRECISION"
-    case SQLTypes.NUMERIC  => "NUMBER"
+    case SQLTypes.CHAR => "CHAR(1 CHAR)"
+    case SQLTypes.DATE => "DATE"
+    case SQLTypes.INTEGER => "NUMBER(10,0)"
+    case SQLTypes.NUMERIC  => "NUMBER(10,0)"
+    case SQLTypes.SMALLINT => "NUMBER(5,0)"
     case SQLTypes.TIME => "DATE"
-    case SQLTypes.TINYINT => "NUMBER(3)"
+    case SQLTypes.TIMESTAMP => "TIMESTAMP"
+    case SQLTypes.TINYINT => "NUMBER(3,0)"
     case SQLTypes.VARCHAR => "VARCHAR2(255 BYTE)"
     case _ => super.defaultSqlTypeName(tmd)
   }
 
   class QueryBuilder(tree: Node, state: CompilerState) extends super.QueryBuilder(tree, state) {
     override protected val concatOperator = Some("||")
+    override protected val scalarFrom = Some("DUAL")
 
+    // http://stackoverflow.com/questions/7480243/sql-oracle-order-by-and-limit
     // http://stackoverflow.com/questions/2912144/alternatives-to-limit-and-offset-for-paging-in-oracle
-    override protected def buildFetchOffsetClause(fetch: Option[Long], offset: Option[Long]) = (fetch, offset) match {
-      case (Some(t), Some(d)) => b" limit $t offset $d"
-      case (Some(t), None   ) => b" limit $t"
-      case (None,    Some(d)) => b" offset $d"
-      case _ =>
-    }
+    // http://stackoverflow.com/questions/7326885/limit-offset-in-oracle-11g
+    // https://github.com/mysema/querydsl/issues/140
+    override protected def buildFetchOffsetClause(fetch: Option[Long], offset: Option[Long]) = {
+      // var sql_ = sql.trim()
 
-    def limitString(sql: String, hasOffset: Boolean): String = {
-       var sql_ = sql.trim()
-
-      var isForUpdate = false;
-      if ( sql.toLowerCase().endsWith(" for update") ) {
-        sql_ = sql_.substring( 0, sql.length()-11 )
-        isForUpdate = true
+      var isForUpdate = false
+      if (b.toString.toLowerCase().endsWith(" for update") ) {
+        (fetch, offset) match {
+          case (Some(t), Some(d)) =>
+            b =+ "select * from ( "
+            b" ) where rownum_ <= $t and rownum_ > $d"
+          case (Some(t), None   ) =>
+              b =+ "select * from ( "
+              b" ) where rownum <= $t"
+          case (None,    Some(d)) =>
+              b =+ "select * from ( "
+              b" ) where  rownum_ > $d"
+          case _ =>
+        }
+      } else {
+        (fetch, offset) match {
+          case (Some(t), Some(d)) =>
+            b =+ "select * from ( select row_.*, rownum rownum_ from ( "
+            b" ) row_ ) where rownum_ <= $t and rownum_ > $d"
+          case (Some(t), None   ) =>
+            b =+ "select * from ( select row_.*, rownum rownum_ from ( "
+            b" ) row_ ) where rownum <= $t"
+          case (None,    Some(d)) =>
+            b =+ "select * from ( select row_.*, rownum rownum_ from ( "
+            b" ) row_ ) where  rownum_ > $d"
+          case _ =>
+        }
       }
-
-      val pagingSelect: StringBuilder = new StringBuilder( sql.length()+100 )
-      if (hasOffset) {
-        pagingSelect.append("select * from ( select row_.*, rownum rownum_ from ( ")
-      }
-      else {
-        pagingSelect.append("select * from ( ")
-      }
-      pagingSelect.append(sql)
-      if (hasOffset) {
-        pagingSelect.append(" ) row_ ) where rownum_ <= ? and rownum_ > ?")
-      }
-      else {
-        pagingSelect.append(" ) where rownum <= ?")
-      }
-
-      if (isForUpdate ) {
-        pagingSelect.append( " for update" )
-      }
-
-      return pagingSelect.toString()
     }
 
     override def expr(n: Node, skipParens: Boolean = false) = n match {
@@ -117,7 +140,9 @@ trait OracleDriver extends JdbcDriver { driver =>
   }
 
   class TableDDLBuilder(table: Table[_]) extends super.TableDDLBuilder(table) {
-   /* override protected def createIndex(idx: Index) = {
+    private var countAutoIncrement = 0
+
+   override protected def createIndex(idx: Index) = {
       if(idx.unique) {
         /* Create a UNIQUE CONSTRAINT (with an automatically generated backing
          * index) because Derby does not allow a FOREIGN KEY CONSTRAINT to
@@ -129,45 +154,142 @@ trait OracleDriver extends JdbcDriver { driver =>
         sb append ")"
         sb.toString
       } else super.createIndex(idx)
-    } */
+    }
 
     override def createPhase1 = super.createPhase1 ++ columns.flatMap {
       case cb: ColumnDDLBuilder => cb.createLobTrigger(table.tableName)
     }
-    //test2: create table "a" ("id" INTEGER NOT NULL,"a" VARCHAR2 DEFAULT 'foo' NOT NULL,"b" NUMBER(1) DEFAULT true)
-    //> create table "a" ("id" INTEGER NOT NULL,"a" VARCHAR2(255 BYTE) DEFAULT 'foo' NOT NULL,"b" NUMBER(1) DEFAULT 1)
-     // CONSTRAINT supplier_pk PRIMARY KEY (supplier_id, supplier_name)
+
+    // autoIncr -> sequence + trigger
+    override def createPhase2 = super.createPhase2 ++ createAutoIncSequencesAndTriggers()
+
+    override def dropPhase2 = super.createPhase2 ++ createAutoIncSequencesAndTriggers()
+
+    def initCountAutoIncrement {
+      this.countAutoIncrement = 0
+      // todo: columns.forall( column => column.autoIncrement) countAutoIncrement = countAutoIncrement + 1
+      for (column <- columns) {
+        if (column.autoIncrement)
+          this.countAutoIncrement = this.countAutoIncrement + 1
+      }
+    }
+
+   def createAutoIncSequencesAndTriggers(): Iterator[String] = {
+      val lb = new ListBuffer[String]
+      initCountAutoIncrement
+
+      if (this.countAutoIncrement == 0) return lb.toIterator
+      val lb2 = new ListBuffer[String]
+
+     var name = table.tableName
+       //  columns.forall( column => column.autoIncrement) match case n if =>
+     for (column <- columns) {
+       if (column.autoIncrement) {
+         // column: FieldSymbol
+         if (countAutoIncrement > 1) name = name + "_" + column.getName
+         lb += "create sequence " + name + "_seq start with 1 increment by 1 nomaxvalue"
+
+         if (countAutoIncrement == 1) {
+           lb += "create trigger " + table.tableName + "_trigger before insert on " + table.tableName +
+             " for each row begin select " + name + "_seq.nextval into :new." + column.getName + " from dual; end"
+           return lb.toIterator
+         } else {
+           lb2 += "select " + name + "_seq.nextval into :new." + column.getName + " from dual;"
+         }
+       }
+     }
+
+    lb += "create trigger " + table.tableName + "_trigger before insert on " + table.tableName + " for each row begin "
+    lb ++ lb2
+    lb += "end;"
+    lb.toIterator
+  }
+
+   def dropAutoIncTriggersAndSequences(): Iterable[String] = {
+     val lb = new ListBuffer[String]
+     initCountAutoIncrement
+
+     if (this.countAutoIncrement == 0) return lb.toIterable
+     val lb2 = new ListBuffer[String]
+
+     var name = table.tableName
+     lb += "drop trigger " + name + "_seq"
+     //  columns.forall( column => column.autoIncrement) match case n if =>
+     for (column <- columns.toList.reverse) {
+       if (column.autoIncrement) {
+         if (countAutoIncrement > 1) name = name + "_" + column.getName
+         lb += "drop sequence " + name + "_seq"
+       }
+     }
+     lb.toIterable
+   }
 
     override def dropPhase1 = {
       val dropLobs = columns.flatMap {
         case cb: ColumnDDLBuilder => cb.dropLobTrigger(table.tableName)
       }
-      if(dropLobs.isEmpty) super.dropPhase1
-      else Seq("delete from "+quoteIdentifier(table.tableName)) ++ dropLobs ++ super.dropPhase1
+      if(dropLobs.isEmpty) {
+        if (this.countAutoIncrement == 0) {
+          super.dropPhase1
+        } else {
+          dropAutoIncTriggersAndSequences ++ super.dropPhase1
+        }
+      } else Seq("delete from "+quoteIdentifier(table.tableName)) ++ dropLobs ++ super.dropPhase1
+    }
+
+    override private[driver] def addForeignKey(fk: ForeignKey[_ <: TableNode, _], sb: StringBuilder) {
+      sb append "constraint " append quoteIdentifier(fk.name) append " foreign key("
+      addForeignKeyColumnList(fk.linearizedSourceColumns, sb, table.tableName)
+      sb append ") references " append quoteIdentifier(fk.targetTable.tableName) append "("
+      addForeignKeyColumnList(fk.linearizedTargetColumnsForOriginalTargetTable, sb, fk.targetTable.tableName)
+      if (fk.onUpdate.action==ForeignKeyAction.NoAction) {
+        sb append ")"
+      } else {
+        sb append ") on update " append fk.onUpdate.action
+        sb append " on delete " append fk.onDelete.action
+      }
     }
   }
 
   class ColumnDDLBuilder(column: FieldSymbol) extends super.ColumnDDLBuilder(column) {
+
     override def appendColumn(sb: StringBuilder) {
       sb append quoteIdentifier(column.name) append ' '
-      if(autoIncrement) {
+     /* if(autoIncrement) {
         autoIncrement = false
-      }
-      else sb append sqlType
+      }*/
+      sb append sqlType
+      if (sqlType.isEmpty) sb append SQLTypes.BIGINT // id
       appendOptions(sb)
+    }
+
+    override protected def appendOptions(sb: StringBuilder) {
+      if(defaultLiteral ne null) {
+        if ((defaultLiteral == true) || (defaultLiteral == "TRUE")) sb append " DEFAULT 1"
+        else if ((defaultLiteral == false) || (defaultLiteral == "FALSE"))  sb append " DEFAULT 0"
+        else sb append " DEFAULT " append defaultLiteral
+      }
+    // todo: Auto create Sequence for the column
+    //  if(autoIncrement) println("ERROR autoIncrement not supported for column:\n"+column.name
+    //                           +". You must use Sequences:\n"+sb.toString)
+
+      if(notNull) sb append " NOT NULL"
+      else if(sqlType.toUpperCase == "TIMESTAMP") sb append " NULL"
+
+      if(primaryKey) sb append " PRIMARY KEY"
     }
 
     def lobTrigger(tname: String) =
       quoteIdentifier(tname+"__"+quoteIdentifier(column.name)+"_lob")
 
     def createLobTrigger(tname: String): Option[String] =
-      if(sqlType == "lo") Some(
+      if(sqlType == "BLOB") Some(
         "create trigger "+lobTrigger(tname)+" before update or delete on "+
         quoteIdentifier(tname)+" for each row execute procedure lo_manage("+quoteIdentifier(column.name)+")"
       ) else None
 
     def dropLobTrigger(tname: String): Option[String] =
-      if(sqlType == "lo") Some(
+      if(sqlType == "BLOB") Some(
         "drop trigger "+lobTrigger(tname)
       ) else None
   }
@@ -189,12 +311,22 @@ trait OracleDriver extends JdbcDriver { driver =>
        * workaround available AFAICT. */
       if(seq._cycle) b append " CYCLE"
       DDL(b.toString, "DROP SEQUENCE " + quoteIdentifier(seq.name))
+
+      // select sequence_name from user_sequences
     }
   }
 
   class JdbcTypes extends super.JdbcTypes {
+    override val booleanJdbcType = new BooleanJdbcType
     override val byteArrayJdbcType = new ByteArrayJdbcType
     override val uuidJdbcType = new UUIDJdbcType
+
+      /* SQLite does not have a proper BOOLEAN type. The suggested workaround is
+       * INTEGER with constants 1 and 0 for TRUE and FALSE. */
+      class BooleanJdbcType extends super.BooleanJdbcType {
+        override def sqlTypeName = "NUMBER(1)"
+        override def valueToSQLLiteral(value: Boolean) = if(value) "1" else "0"
+      }
 
     class ByteArrayJdbcType extends super.ByteArrayJdbcType {
       override val sqlType = SQLTypes.BLOB
@@ -213,6 +345,7 @@ trait OracleDriver extends JdbcDriver { driver =>
       override def nextValue(r: PositionedResult) = r.nextObject().asInstanceOf[UUID]
       override def updateValue(v: UUID, r: PositionedResult) = r.updateObject(v)
       override def valueToSQLLiteral(value: UUID) = "'" + value + "'"
+      // "select rawtohex(sys_guid()) from dual"
     }
   }
 }
